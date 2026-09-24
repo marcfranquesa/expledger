ed5efa0bd135da31dafbf306a17734d9997760f0
Marc Franquesa Monés <git@marcfranquesa.com>
2026-09-24 16:12:23 -0400

    fix(cli): replace build snapshots atomically

---
+++ internal/cli/build_umask_test.go
@@ -0,0 +1,38 @@
+//go:build unix
+
+package cli_test
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/marcfranquesa/expledger/internal/cli"
+)
+
+func TestBuildHonorsUmask(t *testing.T) {
+	if os.Getenv("EXPLEDGER_TEST_UMASK") != "1" {
+		cmd := exec.Command(os.Args[0], "-test.run=^TestBuildHonorsUmask$")
+		cmd.Env = append(os.Environ(), "EXPLEDGER_TEST_UMASK=1")
+		if output, err := cmd.CombinedOutput(); err != nil {
+			t.Fatalf("umask subprocess: %v\n%s", err, output)
+		}
+		return
+	}
+	syscall.Umask(0o077)
+	root := t.TempDir()
+	git(t, root, "init", "--quiet", "--initial-branch=main")
+	git(t, root, "remote", "add", "origin", "https://github.com/owner/repo.git")
+	var stdout bytes.Buffer
+	if err := cli.Run([]string{"build"}, root, time.Time{}, &stdout); err != nil {
+		t.Fatal(err)
+	}
+	info, err := os.Stat(filepath.Join(root, "dist", "index.html"))
+	if err != nil || info.Mode().Perm() != 0o600 {
+		t.Fatalf("umask not respected: %v, %v", info, err)
+	}
+}
